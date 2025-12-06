70719ba92f7a8d9e63214958e8169b363d3c9035
Lexi Larkin <lexi@qixils.dev>
2025-12-06 00:04:49 -0500

    feat: portfolio updates

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -12,32 +12,95 @@ useSeoMeta({
     <h1>Portfolio</h1>
     <p>
       Hi! Welcome to my developer portfolio.
-      Here you'll find most of my completed projects from the past few years,
-      from full-stack websites to Minecraft mods to Discord bots and everything in-between.
+      Here you'll find most of my completed projects from the past few years.
     </p>
 
     <h2>Websites</h2>
     <p>
-      When I first started out as a developer, creating websites always seemed like a daunting task. However, with the
-      advent of web frameworks like <a href="https://react.dev/">React</a>, <a href="https://vuejs.org/">Vue</a>, and
-      <a href="https://svelte.dev/">Svelte</a>, I've learned that developing a website can be fun and easy, so much so
-      that I can now create a small website for a project (like this one!) in only a weekend.
+      Full-stack web development is my greatest passion,
+      ranging from traditional websites
+      to less common uses like Electron apps and livestreaming overlays.
     </p>
 
     <div class="portfolio-gallery">
+      <!-- Projects @ Crowd Control -->
+      <div class="card">
+        <p class="title">Projects @ Crowd Control</p>
+        <p class="subtitle">
+          Written in TypeScript with Electron, Vue, and Trpc.
+          Employed from 2024-present.
+          <a target="_blank" href="https://crowdcontrol.live">Homepage</a>
+        </p>
+        <p>
+          After 3 years of contracting for Crowd Control's Minecraft mod,
+          I was hired on part-time in 2024 and later full-time as a Full-Stack Software Architect
+          for the Crowd Control desktop app, servers, and other frontends.
+          During my tenure I have created the TikTok integration and its several iterations,
+          multiplayer Lobbies,
+          a massive public competition hosted on those Lobbies,
+          Paid-only effects,
+          a new subscription tier,
+          client and server optimizations,
+          a unified component system,
+          and much more.
+        </p>
+      </div>
+      <!-- Speedrun Archive Club -->
+      <div class="card">
+        <p class="title">Speedrun Archive Club</p>
+        <p class="subtitle">
+          Written in TypeScript with Vue and Trpc.
+          Developed in 2025.
+          <a target="_blank" href="https://github.com/qixils/SpeedrunArchiveClub">Source Code</a>,
+          <a target="_blank" href="https://archive.speedrun.club/">Website</a>,
+          <a target="_blank" href="https://github.com/qixils/ArchiveSpeedruns">Scripts</a>
+        </p>
+        <p>
+          Following Twitch's announcement in early 2025 that they would be enacting a mass purge of
+          old video content, I discovered over 100K speedruns submitted to various speedrun sites
+          that were pending deletion and worked with arkiver of the
+          <a target="_blank" href="https://wiki.archiveteam.org/">Archive Team</a>
+          to save them along with 600K+ other popular videos.
+          The website allows anyone to easily find and download deleted videos without needing to
+          learn the intricacies of the Internet Archive's Wayback Machine storage format
+          and Twitch's web of GraphQL queries.
+        </p>
+      </div>
+      <!-- Livestream Broadcast Graphics -->
+      <div class="card">
+        <p class="title">Livestream Broadcast Graphics</p>
+        <p class="subtitle">
+          Written in TypeScript/Vue.
+          Developed in 2024-present.
+          Demo Video
+          <a target="_blank" href="https://www.youtube.com/watch?v=_6J4j3OUIvA">#1</a>,
+          <!-- <a target="_blank" href="">#2</a>, -->
+          <a target="_blank" href="https://github.com/qixils/lss64">Source Code</a>
+        </p>
+        <p class="description">
+          In 2024 I hosted several tournaments using
+          <a target="_blank" href="https://nodecg.dev/">NodeCG</a>,
+          <a target="_blank" href="https://github.com/speedcontrol/nodecg-speedcontrol">speedcontrol</a>,
+          and custom OBS graphic overlays I developed in Vue.
+          In 2025 I expanded the overlays for an event produced by my employer
+          to source in real-time placement data,
+          and further developed its graphics and real-time Twitch data inputs
+          for an upcoming Twitch subathon.
+        </p>
+      </div>
       <!-- Speedrun VOD Club -->
       <div class="card">
         <p class="title">Speedrun VOD Club</p>
         <p class="subtitle">
           Written in Kotlin/Ktor and TypeScript/SvelteKit.
-          Developed in 2019-2023.
+          Developed in 2019-present.
           <a target="_blank" href="https://github.com/qixils/kgdq">Source Code</a>,
           <a target="_blank" href="https://vods.speedrun.club/">Website</a>
         </p>
         <p class="description">
           Together with my friend <a href="https://dunkyl.net">Dunkyl</a>, we wrote a website that allows you to share
           and watch archived videos of speedruns from charity marathons, like
-          <a href="https://gamesdonequick.com">Games Done Quick</a>.
+          <a href="https://gamesdonequick.com">Games Done Quick</a> which has previously promoted the site.
           The backend is written in Kotlin using the Ktor framework.
           The <a href="https://vods.speedrun.club/">web frontend</a> is written in TypeScript using the SvelteKit framework,
           while the <a href="https://reddit.com/u/VODThread">Reddit frontend</a> is written in Kotlin.
@@ -96,8 +159,10 @@ useSeoMeta({
           <a target="_blank" href="https://mlb.qixils.dev">Website</a>
         </p>
         <p class="description">
-          Created in 2022 for my parents, this website allows users to view the season statistics of the current batter
-          and pitcher of any Major League Baseball game. It was developed using the MLB Stats API and is written in
+          Created in 2022 when my parents complained to me about not knowing how good the players in the finals were,
+          this website allows users to view the season statistics of the current batter
+          and pitcher of any Major League Baseball game.
+          It was developed using the (reverse-engineered!) MLB Stats API and is written in
           TypeScript using the SvelteKit framework.
         </p>
       </div>
@@ -110,7 +175,7 @@ useSeoMeta({
           <a target="_blank" href="https://github.com/qixils/debut/">Source Code</a>
         </p>
         <p class="description">
-          Twitch extension created for my debut <a href="https://twitch.tv/lexikiq">Twitch stream</a>.
+          Twitch overlay extension created for my debut <a href="https://twitch.tv/lexikiq">Twitch stream</a>.
           Powered by a Kotlin backend using the Ktor framework, and a TypeScript frontend using the SvelteKit framework.
         </p>
       </div>
@@ -118,10 +183,8 @@ useSeoMeta({
 
     <h2>Minecraft</h2>
     <p>
-      Crafting unique experiences in this block game has been a large focus of mine for the past few years. From one-off
-      <a href="https://fabricmc.net/">Fabric mods</a> to entire games developed with the
-      <a href="https://papermc.io/">Paper API</a>,
-      it's hard to find an aspect of the classic game that I haven't worked with.
+      Crafting unique experiences in this block game was my first serious introduction to coding.
+      I've worked with every major mod loader and even the game's own data systems.
     </p>
     <div class="portfolio-gallery">
       <!-- Minecraft Crowd Control -->
@@ -130,18 +193,18 @@ useSeoMeta({
         <p class="subtitle">
           Written in Java.
           <a href="https://mccc.qixils.dev/">Website</a> written in TypeScript/Nuxt.
-          Developed from 2021-2023.
+          Developed from 2021-present.
           <a target="_blank" href="https://github.com/qixils/minecraft-crowdcontrol">Source Code</a>
         </p>
         <p class="subtitle"></p>
         <p class="description">
-          Developed in collaboration with <a href="https://crowdcontrol.live">crowdcontrol.live</a>, this Minecraft mod
-          developed for numerous modding frameworks
+          Developed for <a href="https://crowdcontrol.live">crowdcontrol.live</a>, this Minecraft mod
+          targeting several major modding frameworks
           (including <a href="https://papermc.io/">Paper</a>,
           <a href="https://fabricmc.net/">Fabric</a>,
-          and <a href="https://spongepowered.org/">Sponge</a>)
-          allows viewers of livestreams to alter the streamers' game by donating money to the streamers
-          or to a charity of the streamers' choice.
+          and <a href="https://neoforged.net/">NeoForge</a>)
+          allows viewers of livestreams to alter the streamers' game by donating money to the
+          streamers or to a charity of the streamers' choice.
         </p>
       </div>
       <!-- Project Eden -->
@@ -169,13 +232,14 @@ useSeoMeta({
         <p class="title">Mini Mods</p>
         <p class="subtitle">
           Written in Java.
-          Developed in 2023.
+          Developed in 2023-2024.
         </p>
         <p class="description">
-          In 2023, I created several miniature mods for the game, including
+          I created several miniature mods for the game, including
           <a href="https://github.com/qixils/antisocial">Antisocial</a> for hiding other players,
           <a href="https://github.com/qixils/depiglining">Depiglining</a> for reverting nerfs to speedrunning,
-          and <a href="https://github.com/qixils/fahare">Fahare</a> for quickly resetting worlds in multiplayer.
+          <a href="https://github.com/qixils/fahare">Fahare</a> for quickly resetting worlds in multiplayer,
+          and <a href="https://github.com/qixils/scoreboard-stats-import">Scoreboard Stats Import</a> for importing previously obtained statistics.
         </p>
       </div>
       <!-- Paper -->
@@ -188,23 +252,51 @@ useSeoMeta({
         <p class="description">
           I've contributed
           <a href="https://github.com/PaperMC/Paper/pulls?q=is%3Apr+author%3Aqixils+sort%3Aupdated-desc+">
-            numerous pull requests</a> to the Paper server software, as well as to its related projects like
+            numerous pull requests
+          </a> to the Paper server software, as well as to its related projects like
           <a href="https://github.com/PaperMC/Velocity/pull/522">Velocity</a>
           and <a href="https://github.com/KyoriPowered/adventure/pull/393">Adventure</a>,
           to add new features and fix bugs throughout the years.
         </p>
       </div>
-      <!-- TODO more projects. All Obtainable Items? -->
     </div>
 
     <h2>Discord</h2>
     <p>
-      My programming journey has come a long way since my first Discord bot written in C# that was stuffed into one huge file.
-      Since then, I've accrued years of experience in developing bots for Discord, particularly with the
-      <a href="https://discordpy.readthedocs.io/en/latest/">discord.py</a> library, and continue to work on creating my
-      own <a href="https://github.com/quasicolon/quasicolon/">batteries-included bot library for Java</a> in my free time.
+      This chatting platform helps me keep in touch with many of my closest friends,
+      so throughout the years I've created several tools and games to enhance
+      my and my friends' experience.
     </p>
     <div class="portfolio-gallery">
+      <!-- Hidden "Rule" Game -->
+      <div class="card">
+        <div class="title">Hidden "Rule" Game</div>
+        <p class="subtitle">
+          Written in Kotlin.
+          Developed in 2025.
+          <a target="_blank" href="https://github.com/qixils/hidden-rule-game">Source Code</a>
+        </p>
+        <p class="description">
+          Created for my friends' April Fools, this bot assigns secret rules to chatters
+          which must be guessed by other chatters.
+        </p>
+      </div>
+      <!-- demowocracy -->
+      <div class="card">
+        <div class="title">Democracy</div>
+        <p class="subtitle">
+          Written in Kotlin.
+          Developed in 2024.
+          <a target="_blank" href="https://github.com/qixils/demowocwacy">Source Code</a>
+        </p>
+        <p class="description">
+          Created for my friends' April Fools, this bot inspired by Blaseball
+          manages an automated election cycle which
+          allows users to pass one of 39 pre-written "laws" to change something about the server,
+          such as its icon, moderation rules, bots, nicknames, colors, minigames, and more.
+          It was updated with new laws in 2025.
+        </p>
+      </div>
       <!-- semicolon -->
       <div class="card">
         <p class="title">Semicolon</p>
@@ -221,21 +313,6 @@ useSeoMeta({
           configurations, integrates with numerous third-party APIs such as
           <a href="https://perspectiveapi.com/">Perspective</a> to assist with real-time moderation, and features a
           robust localization system which allows the bot to be used by users of any language.
-        </p>
-      </div>
-      <!-- quasicolon -->
-      <div class="card">
-        <p class="title">Quasicord</p>
-        <p class="subtitle">
-          Written in Java.
-          Developed from 2021-2023.
-          <a target="_blank" href="https://github.com/quasicolon/quasicord">Source Code</a>
-        </p>
-        <p class="description">
-          Quasicolon is my work-in-progress batteries-included Java library for Discord bots. The library has been built
-          from the ground up with first-class support for localization, allowing developers to easily develop bots for a
-          wider audience. It also manages common boilerplate such as connecting to and fetching objects from a MongoDB
-          server.
         </p>
       </div>
       <!-- SOTPAL -->
